a7504e69e8568f9ad414f9821c0bf3c99b0d49f8
Charles Wright <cvwright@fastmail.com>
2026-01-17 11:02:19 -0600

    Remove the auto-generated Python client SDK

--- python-sdk/reeeductio_client/__init__.py
+++
@@ -1,8 +0,0 @@
-
-""" A client library for accessing reeeductio: Absurdly Simple Encrypted Spaces """
-from .client import AuthenticatedClient, Client
-
-__all__ = (
-    "AuthenticatedClient",
-    "Client",
-)
--- python-sdk/reeeductio_client/models/capability_op.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class CapabilityOp(str, Enum):
-    CREATE = "create"
-    READ = "read"
-    WRITE = "write"
-
-    def __str__(self) -> str:
-        return str(self.value)
